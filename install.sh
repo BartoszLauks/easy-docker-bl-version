c96e0c19ecc6719e9b4c43d4752648af3e96dd0f
Bartosz Lauks <54795323+BartoszLauks@users.noreply.github.com>
2024-06-06 13:24:50 +0200

    Update install.sh
    fix

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ COMPOSER_ALLOW_SUPERUSER=1 docker compose exec -T appName-php composer self-upda
 COMPOSER_ALLOW_SUPERUSER=1 docker compose exec -T appName-php composer update --no-interaction --classmap-authoritative --optimize-autoloader
 
 docker compose exec -T appName-php php bin/console doctrine:database:create --if-not-exists
-#Uncomment if this is your first migration
+#Uncomment if app have any migration
 #docker compose exec -T appName-php php bin/console doctrine:migrations:migrate --no-interaction
 
 # 1 JWT 
